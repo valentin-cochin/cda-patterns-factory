0ac33287027ee825e739a27f71104c4cfef617da
valentin-cochin <51408988+valentin-cochin@users.noreply.github.com>
2021-01-15 13:45:43 +0100

    Update Application.java

--- src/cda/exec/Application.java
+++ src/cda/exec/Application.java
@@ -20,11 +20,6 @@ public final class Application {
         }
     }
 
-    /**
-     * All of the client code should work with factories and products through
-     * abstract interfaces. This way it does not care which factory it works
-     * with and what kind of product it returns.
-     */
     static void runBusinessLogic() {
         dialog.render();
     }
